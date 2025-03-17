b5c275689ae4763d491a9a060aff0743cef0eaf6
Alejandro P <alepuente181@gmail.com>
2025-03-17 20:14:17 +0100

    Clases Nave y NaveAmiga

---
+++ core/src/main/java/com/politecnicomalaga/spaceinvaders/NaveAmiga.java
@@ -0,0 +1,19 @@
+package com.politecnicomalaga.spaceinvaders;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NaveAmiga extends Nave {
+    private int iVidas;
+
+    public NaveAmiga(int iPosX, int iPosY, int vel, int iAncho, int iAlto, Texture imagen, boolean estaVivo, ObjetoVolador.direccion direccion, int iVidas) {
+        super(iPosX, iPosY, vel, iAncho, iAlto, imagen, estaVivo, direccion);
+        this.iVidas = iVidas;
+    }
+}
+
+
