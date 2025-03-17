640e50e3039273f4153a7a0ed612476b6203077d
Alejandro P <alepuente181@gmail.com>
2025-03-17 20:15:19 +0100

    Clase disparo

---
+++ core/src/main/java/com/politecnicomalaga/spaceinvaders/Disparo.java
@@ -0,0 +1,15 @@
+package com.politecnicomalaga.spaceinvaders;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Disparo extends ObjetoVolador{
+
+    private boolean esAmigo;
+
+    public Disparo(int iPosX, int iPosY, int vel, int iAncho, int iAlto, Texture imagen) {
+        super(iPosX, iPosY, vel, iAncho, iAlto, imagen);
+    }
+
+}
+
